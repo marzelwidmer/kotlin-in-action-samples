7246fc2b2dcc1daa9934a602954b1a339b6d0976
Marcel Widmer <marzelwidmer@gmail.com>
2019-07-18 15:13:34 +0200

    4.3 Data classes and class delegation (data class)

--- src/data/Main.kt
+++ src/data/Main.kt
@@ -10,16 +10,16 @@ fun main() {
     println("Contains ?  ${processed.contains(Client(name = "Alice", postalCode = 342562))}")
 }
 
-class Client(val name: String, val postalCode: Int){
-    override fun toString(): String {
-        return "Client(name=$name, postalCode=$postalCode)"
-    }
-
-     override fun equals(other: Any?): Boolean {
-         if(other == null || other !is Client)
-             return false
-         return name == other.name && postalCode == other.postalCode
-     }
-
-    override fun hashCode(): Int = name.hashCode() * 31 + postalCode
+data class Client(val name: String, val postalCode: Int){
+//    override fun toString(): String {
+//        return "Client(name=$name, postalCode=$postalCode)"
+//    }
+//
+//     override fun equals(other: Any?): Boolean {
+//         if(other == null || other !is Client)
+//             return false
+//         return name == other.name && postalCode == other.postalCode
+//     }
+//
+//    override fun hashCode(): Int = name.hashCode() * 31 + postalCode
 }
